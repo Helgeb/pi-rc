4ee2cff0d8aabe3b19e3ef17df9d7c41a5a9cec0
Helge Boeschen <helge.boeschen@ewe.de>
2021-03-14 20:33:13 +0100

    frontend stream + api

--- pi-rc-frontend/src/presentational/pi-rc.tsx
+++ pi-rc-frontend/src/presentational/pi-rc.tsx
@@ -7,13 +7,20 @@ const PIRC = (props: PIRCProps) => {
 
   const [data, setData] = useState("");
 
+  const onClick = async () => {
+    await getHelloWorld(piUri);
+  };
+
   return (
     <div>
-      <p> {data}</p>
-      <button
-        className="bg-green-400 rounded-full"
-        onClick={async () => setData(await getHelloWorld(piUri))}
-      >
+      {/* <p> {data}</p> */}
+      <img
+        src={`http://${piUri}:8001/`}
+        width="640"
+        height="480"
+        alt="no stream"
+      />
+      <button className="bg-green-400 rounded-full" onClick={onClick}>
         Fetch data
       </button>
     </div>
